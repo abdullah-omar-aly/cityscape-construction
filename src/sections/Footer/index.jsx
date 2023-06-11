53104123d911c358d24156759dadd888e6e40d76
Abdullah Omar <81812150+abdullah-omar-aly@users.noreply.github.com>
2023-06-11 07:26:20 +0300

    remove unused code in Footer

--- src/sections/Footer/index.jsx
+++ src/sections/Footer/index.jsx
@@ -1,31 +1,25 @@
-"use client"
-
-
-import { FacebookIcon, InstgramIcon, TwitterIcon } from '@/icons'
-import React, { useRef } from 'react'
-
+import { FacebookIcon, InstgramIcon, TwitterIcon } from "@/icons";
+import React from "react";
 
 const Footer = () => {
-    const footerRef = useRef()
+  return (
+    <footer
+      className="bg-gray-900 flex  justify-center items-center py-3 "
+      style={{ backgroundColor: "#212122" }}
+    >
+      <div className=" flex justify-center items-center text-main-primary lg:text-white">
+        <a href="#" className="p-2 text-lg">
+          <TwitterIcon />
+        </a>
+        <a href="#" className="p-2 text-lg">
+          <InstgramIcon />
+        </a>
+        <a href="#" className="p-2 text-lg">
+          <FacebookIcon />
+        </a>
+      </div>
+    </footer>
+  );
+};
 
-
-    return (
-        <footer ref={footerRef} className='bg-gray-900 flex  justify-center items-center py-3 ' style={{ backgroundColor: '#212122' }}>
-            <div className=" flex justify-center items-center text-main-primary lg:text-white">
-                <a href="#" className="p-2 text-lg">
-                    <TwitterIcon />
-                </a>
-                <a href="#" className="p-2 text-lg">
-                    <InstgramIcon />
-                </a>
-                <a href="#" className="p-2 text-lg">
-                    <FacebookIcon />
-                </a>
-
-            </div>
-
-        </footer>
-    )
-}
-
-export default Footer
+export default Footer;
